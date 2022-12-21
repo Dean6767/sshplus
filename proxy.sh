c11cc238c74b9f38d7204b9e9b609a4b58507a2a
Dean Winchester <80933973+Dean6767@users.noreply.github.com>
2022-12-21 00:15:36 -0300

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+     apt update
+	 cd /root
+	 echo "Instalando Proxy"
+	 curl -L https://mirrors.host900.com/https://raw.githubusercontent.com/snail007/goproxy/master/install_auto.sh | bash
+	 echo "Iniciando Proxy na porta 80"
+	 proxy http -t tcp -p "0.0.0.0:80" --daemon
+	 echo "PROXY HTTP RODANDO!"
